e5ab4d39a2e629f6cdd872f92779a6ab00cf0635
hoangvd <78503846+rhynofuyu@users.noreply.github.com>
2025-05-06 13:01:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>  // Add TTF library
+#include <SDL2/SDL_ttf.h> 
 #include <iostream>
 #include <vector>
 #include <string>
