7326550fdc7f7542a3fc56da4687fe5387105841
hoangvd <78503846+rhynofuyu@users.noreply.github.com>
2025-05-22 12:52:08 +0700

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-g++ -Wall -std=c++17 -Dmain=SDL_main -I./src/include main.cpp src/game_structures.cpp src/texture_manager.cpp src/solver.cpp src/game_resources.cpp src/renderer.cpp -o main.exe -L./src/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
-
-if %ERRORLEVEL% EQU 0 (
-    echo Build successful! Running the game...
-    main.exe
-)
